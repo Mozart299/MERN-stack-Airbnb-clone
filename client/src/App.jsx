3eb71803ec4ee72087061e905acd8e713fbe3359
Peter Kyeyune <149464110+LOGOS-APPS@users.noreply.github.com>
2024-01-06 13:46:30 +0300

    made several changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import './App.css';
 import Layout from './Layout';
 import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+
+axios.defaults.baseURL = 'http://localhost:4000';
 
 function App() {
 
@@ -11,6 +14,7 @@ function App() {
       <Route path='/' element={<Layout />}>
         <Route index element={<IndexPage />} />
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
       </Route>
     </Routes>
 
